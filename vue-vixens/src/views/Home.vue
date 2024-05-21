11f22f60d4d6920b2740b5c5787fbae8b5acf93c
PEEWEE\petra <petravos70@gmail.com>
2024-05-21 21:07:04 +0200

    Add translations.

--- vue-vixens/src/views/Home.vue
+++ vue-vixens/src/views/Home.vue
@@ -12,8 +12,8 @@
       <h2>{{ $t("home.donate") }}</h2>
     </div>
     <div class="panel tall-panel light-brown">
-      <h2>{{ $t("home.Adoptable-Pets") }}</h2>
-      <p>Fisher, Chihuahua, age 3</p>
+      <h2>{{ $t("home.adoptable-pets") }}</h2>
+      <p>{{ $t("home.animal-name") }}</p>
       <img
         src="https://raw.githubusercontent.com/VueVixens/projects/master/petshop/images/chihuahua.jpg"
       >
@@ -23,26 +23,26 @@
       <h2>{{ $t("home.contact") }} </h2>
     </div>
     <div class="panel tall-panel dark-mint">
-      <h2>Pet of the Month</h2>
-      <p>Meet Stanley, A young French Bulldog</p>
+      <h2>{{ $t("home.pet-of-the-Month") }}</h2>
+      <p>{{ $t("home.introduce-pet") }}</p>
       <img
         src="https://raw.githubusercontent.com/VueVixens/projects/master/petshop/images/bulldog.jpg"
       >
     </div>
     <div class="panel tall-panel light-mint">
-      <h2>Success Stories</h2>
-      <p>Bennie found his forever home!</p>
+      <h2>{{ $t("home.success-Stories") }}</h2>
+      <p>{{ $t("home.forever-home") }}</p>
       <img
         src="https://raw.githubusercontent.com/VueVixens/projects/master/petshop/images/collie.jpg"
       >
     </div>
 
     <div class="panel bisque">
-      <h2>Special Events</h2>
+      <h2>{{ $t("home.special-events") }}</h2>
     </div>
 
     <div class="panel bisque">
-      <h2>Learn About Pet Ownership</h2>
+      <h2>{{ $t("home.pet-ownership") }}</h2>
     </div>
   </div>
 </template>
